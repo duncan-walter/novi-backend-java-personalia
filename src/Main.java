b985604da4ca62015b0990fa89400914f667e078
Duncan Walter <dwalter@snelstart.nl>
2025-09-10 15:04:59 +0200

    added remaning set variable methods and updated menu accordingly

--- src/Main.java
+++ src/Main.java
@@ -15,9 +15,14 @@ public class Main {
 
     public static void main(String[] args) {
         printMenu();
-        setName();
 
-//        Roep hier de andere methodes aan
+        setName();
+        setAge();
+        setEmail();
+        setAddress();
+        setPhone();
+        setHeight();
+        setWeight();
 
         printProfile();
         System.out.println("Afsluiten. Tot ziens!");
@@ -28,8 +33,12 @@ public class Main {
         System.out.println("Welkom bij de profiel app");
         System.out.println("Deze app zal het volgende uitvoeren:");
         System.out.println(" 1  - Vul naam in");
-//        Vul hier het menu aan
-
+        System.out.println(" 2  - Vul leeftijd in");
+        System.out.println(" 3  - Vul e-mail in");
+        System.out.println(" 4  - Vul adres in");
+        System.out.println(" 5  - Vul telefoonnummer in");
+        System.out.println(" 6  - Vul lengte in (cm)");
+        System.out.println(" 7  - Vul gewicht in (kg)");
     }
 
     private static void printProfile() {
@@ -44,17 +53,43 @@ public class Main {
 
     private static void setName() {
         System.out.print("Voer je volledige naam in: ");
-        String input = scanner.nextLine().trim();
-        name = input;
+        name = scanner.nextLine().trim();
         System.out.println("Naam opgeslagen: " + name);
     }
 
+    private static void setAge() {
+        System.out.print("Voer je leeftijd in: ");
+        age = scanner.nextInt();
+        System.out.println("Leeftijd opgeslagen: " + age);
+    }
 
-//        Schrijf hier de andere methodes
+    private static void setEmail() {
+        System.out.print("Voer je e-mail in: ");
+        email = scanner.nextLine();
+        System.out.println("E-mail opgeslagen: " + email);
+    }
 
+    private static void setPhone() {
+        System.out.print("Voer je telefoonnummer in: ");
+        phone = scanner.nextLine();
+        System.out.println("Telefoonnummer opgeslagen: " + phone);
+    }
 
+    private static void setAddress() {
+        System.out.print("Voer je adres in: ");
+        address = scanner.nextLine();
+        System.out.println("Adres opgeslagen: " + address);
+    }
 
+    private static void setHeight() {
+        System.out.print("Voer je lengte in: ");
+        height = scanner.nextFloat();
+        System.out.println("Lengte opgeslagen: " + height + "cm");
+    }
 
-
-
+    private static void setWeight() {
+        System.out.print("Voer je gewicht in: ");
+        weight = scanner.nextFloat();
+        System.out.println("Gewicht opgeslagen: " + weight + "kg");
+    }
 }
